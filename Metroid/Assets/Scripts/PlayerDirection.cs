de989b49343cb2839e7ef87a00a61109d8553398
taj <ahmadtaj@umich.edu>
2025-01-17 22:43:08 -0500

    Direction done!
    Have the direction for player working!!!!

---
+++ Metroid/Assets/Scripts/PlayerDirection.cs
@@ -0,0 +1,51 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class PlayerDirection : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+    public Sprite spriteLookingForward;
+    public Sprite spriteLookingUpward;
+
+    bool facingRight = true;
+    bool lookingUp = false;
+
+    void Update()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        if (facingRight && horizontalAxis < 0)
+        {
+            facingRight = false;
+            this.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (!facingRight && horizontalAxis > 0)
+        {
+            facingRight = true;
+            this.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        // Vertical: check if up key is held
+        bool holdingUp = Input.GetKey(KeyCode.UpArrow);
+        if (lookingUp && !holdingUp)
+        {
+            lookingUp = false;
+            spriteRenderer.sprite = spriteLookingForward;
+        }
+        else if (!lookingUp && holdingUp)
+        {
+            lookingUp = true;
+            spriteRenderer.sprite = spriteLookingUpward;
+        }
+    }
+
+    public bool isFacingRight()
+    {
+        return facingRight;
+    }
+
+    public bool isLookingUp()
+    {
+        return lookingUp;
+    }
+}
